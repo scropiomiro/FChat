dc4fc522d5882ef9eb375bf7ce831c78c23fb120
scorpiomiro <libraengineer35@gmail.com>
2018-04-09 11:43:36 +0800

    v1.0

---
+++ FChat/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FChat
+//
+//  Created by YVTC on 2018/4/9.
+//  Copyright © 2018年 YVTC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
